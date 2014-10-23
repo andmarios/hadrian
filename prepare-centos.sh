79ab14fb5d510d4dfe04ac4152368115aeda82d3
Marios Andreopoulos <opensource@andmarios.com>
2014-10-23 10:46:42 +0000

    Fix python problems with versioned packages and fix configuration issues.

--- prepare-centos.sh
+++ prepare-centos.sh
@@ -4,6 +4,7 @@
 sudo yum install -y python-devel git gcc-c++
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
-sudo python install Fabric argparse cm_api simplejson python-novaclient
+#sudo pip install Fabric argparse cm_api simplejson python-novaclient
+sudo pip install fabric==1.8.1 paramiko==1.10 argparse cm_api simplejson python-novaclient
 
 # Install hadrian:
